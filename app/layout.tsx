091b21fc096d4f688c6bb69739a6f5dd03ccf5d5
Monarchpainters <info@monarchpainters.co.uk>
2025-09-07 21:46:16 +0530

    fixed favicon issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,11 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#711f50" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
